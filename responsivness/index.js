69747cb9477a8b9ff70f7f597ba9c9ea12d0444e
Mario Kozic <kun.zizinho@gmail.com>
2020-03-04 23:17:18 -0500

    update

---
+++ responsivness/index.js
@@ -0,0 +1,28 @@
+
+//DEKLARIRALI SMO GLOBALNE VARIABLE
+
+
+let displayEl = document.getElementById("output");
+displayEl.style.display = "none";
+
+// VARIABLE U KOJE CEMO POHRANITI KORISNIKOV INPUT
+
+var searchTermsEl = "";
+var numOfRecordsEl = [];
+var startYearEl = "";
+var endYearEl = "";
+  
+
+$(".search").on("click", function(event){
+    
+    
+    searchTermsEl = "";
+    numOfRecordsEl = [];
+    startYearEl = "";
+    endYearEl = "";
+
+})
+
+
+
+
